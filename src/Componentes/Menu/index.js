4dcd49473fd63c9dd021dd5d2235608ca3ff7f3f
Matheus Cassioli <matheuscassioli@hotmail.com>
2023-01-31 21:05:15 -0300

    buildando pathame

--- src/Componentes/Menu/index.js
+++ src/Componentes/Menu/index.js
@@ -1,17 +1,24 @@
-import React from 'react' 
-import { Link } from 'react-router-dom'
+import React from 'react'
+import { Link, useLocation } from 'react-router-dom'
 import styles from "./Menu.module.css"
 import infoNavMenu from "./Menu.nav.json"
 
 export default function Menu() {
+    const location = useLocation();
+
     return (
-       <header>
-         <nav className={styles.navegacao}> 
-                { Object.keys(infoNavMenu).map((key, i) =>
-                        <Link to={infoNavMenu[key].path}  className={styles.link} key={i} >{infoNavMenu[key].route}</Link>
-                    )
-                } 
-        </nav>
-       </header>
+        <header>
+            <nav className={styles.navegacao}>
+                {Object.keys(infoNavMenu).map((key, i) =>
+                    <Link to={infoNavMenu[key].path} className={
+                        `${styles.link} 
+                         ${location.pathname === infoNavMenu[key].routePath ? styles.linkLocalizacao : ''}`}
+                        key={i} >
+                        {infoNavMenu[key].route}
+                    </Link>
+                )
+                }
+            </nav>
+        </header>
     )
 }
