d66fcb5485a5ce5d5cb89c474d38f09e5ac47403
Matheus Cassioli <matheuscassioli@hotmail.com>
2023-02-08 21:16:41 -0300

    build sobremim

--- src/Paginas/SobreMim/index.js
+++ src/Paginas/SobreMim/index.js
@@ -1,8 +1,24 @@
 import React from 'react'
-import AnimatedPageTransition from 'Componentes/AnimatedPageTransition/index' 
+import AnimatedPageTransition from 'Componentes/AnimatedPageTransition/index'
+import PostModelo from 'Componentes/PostModelo'
+import fotoCapa from "../../Assets/fundoCapa.png"
+import styles from "./SobreMim.module.css"
+import minhaFoto from "../../Assets/minhafoto.png" 
+
 export default function SobreMim() {
   return (
-    <AnimatedPageTransition> 
+    <AnimatedPageTransition>
+      <PostModelo fotoCapa={fotoCapa} titulo={'Sobre Mim'} >  
+        <h3 className={styles.subTitulo}>Olá, sou o Matheus Cassioli </h3>
+        <img src={minhaFoto} alt={'Foto de Matheus Cassioli'} className={styles.minhaFoto} />
+        <p className={styles.textoSobreMim}>Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor desconhecido pegou uma bandeja de tipos e os embaralhou para fazer um livro de modelos de tipos. Lorem Ipsum sobreviveu não só a cinco séculos, como também ao salto para a editoração eletrônica, permanecendo essencialmente inalterado. Se popularizou na década de 60, quando a Letraset lançou decalques contendo passagens de Lorem Ipsum, e mais recentemente quando passou a ser integrado a softwares de editoração eletrônica como Aldus PageMaker.</p>
+        <p className={styles.textoSobreMim}>Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor desconhecido pegou uma bandeja de tipos e os embaralhou para fazer um livro de modelos de tipos. Lorem Ipsum sobreviveu não só a cinco séculos, como também ao salto para a editoração eletrônica, permanecendo essencialmente inalterado. Se popularizou na década de 60, quando a Letraset lançou decalques contendo passagens de Lorem Ipsum, e mais recentemente quando passou a ser integrado a softwares de editoração eletrônica como Aldus PageMaker.</p>
+        <p className={styles.textoSobreMim}>Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor desconhecido pegou uma bandeja de tipos e os embaralhou para fazer um livro de modelos de tipos. Lorem Ipsum sobreviveu não só a cinco séculos, como também ao salto para a editoração eletrônica, permanecendo essencialmente inalterado. Se popularizou na década de 60, quando a Letraset lançou decalques contendo passagens de Lorem Ipsum, e mais recentemente quando passou a ser integrado a softwares de editoração eletrônica como Aldus PageMaker.</p>
+        <p className={styles.textoSobreMim}>Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor desconhecido pegou uma bandeja de tipos e os embaralhou para fazer um livro de modelos de tipos. Lorem Ipsum sobreviveu não só a cinco séculos, como também ao salto para a editoração eletrônica, permanecendo essencialmente inalterado. Se popularizou na década de 60, quando a Letraset lançou decalques contendo passagens de Lorem Ipsum, e mais recentemente quando passou a ser integrado a softwares de editoração eletrônica como Aldus PageMaker.</p>
+        <p className={styles.textoSobreMim}>Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor desconhecido pegou uma bandeja de tipos e os embaralhou para fazer um livro de modelos de tipos. Lorem Ipsum sobreviveu não só a cinco séculos, como também ao salto para a editoração eletrônica, permanecendo essencialmente inalterado. Se popularizou na década de 60, quando a Letraset lançou decalques contendo passagens de Lorem Ipsum, e mais recentemente quando passou a ser integrado a softwares de editoração eletrônica como Aldus PageMaker.</p>
+      </PostModelo>
+
+
     </AnimatedPageTransition>
   )
 } 
