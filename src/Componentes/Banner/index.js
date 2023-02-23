92eafde04b95603c55c078b9abc6140ae5bb17fe
Matheus Cassioli <matheuscassioli@hotmail.com>
2023-02-23 20:10:23 -0300

    arrumando redes

--- src/Componentes/Banner/index.js
+++ src/Componentes/Banner/index.js
@@ -3,21 +3,22 @@ import styles from "./Banner.module.css"
 import dados from "./DadosBanner.json"
 // import circuloColors from "Assets/circulo colorido.png"
 import minhafoto from "Assets/minhafoto.png"
+import { Link } from 'react-router-dom'
 
 export default function Banner() {
     return (
         <section className={styles.banner}>
             <div className={styles.left}>
                 <h1 className={styles.titulo}>{dados.titulo}</h1>
-                <p className={styles.texto}>{dados.apresentacao}</p> 
+                <p className={styles.texto}>{dados.apresentacao}</p>
             </div>
             <div className={styles.right}>
                 {/* <img className={styles.circuloColorido} src={circuloColors} aria-hidden={true} alt="Circulo Colorido" /> */}
                 <img className={styles.minhafoto} src={minhafoto} alt="foto de perfil Matheus" />
                 <div className={styles.redes}>
-                    <div className={styles.github}></div>
-                    <div className={styles.linkedin}></div>
-                    <div className={styles.whatsapp}></div>
+                    <Link target='_blank' to="https://github.com/matheuscassioli" className={styles.github}></Link>
+                    <Link target='_blank' to="https://www.linkedin.com/in/matheuscss/" className={styles.linkedin}></Link>
+                    <Link target='_blank' to="https://api.whatsapp.com/send?phone=5511941703623" className={styles.whatsapp}></Link>
                 </div>
             </div>
         </section>
